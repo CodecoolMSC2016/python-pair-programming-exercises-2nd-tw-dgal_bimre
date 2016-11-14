7533c2ca213b06956e92ff539499f0607fc5e153
Bosnyák Imre Dávid <suti211@gmail.com>
2016-11-14 14:47:12 +0100

    finished palidrome task, he cube is turning....

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,18 @@
-def palindrome(str):
-    return
+def palindrome(string):
+    matchCount = 0
+    lista = string.replace(" ", "").lower()
+    for i in range(len(lista)):
+        if lista[i] == lista[-1 - i]:
+            matchCount += 1
+
+    if matchCount == len(lista):
+        return True
+    else:
+        return False
 
 
 def main():
+    print(palindrome("A nut for a jar of tuna"))
     return
 
 
