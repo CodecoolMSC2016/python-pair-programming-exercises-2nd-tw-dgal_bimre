13a1d4a9eee014e1f758b3beae84d0a558ae5d5a
Daniel Gal <faramirex070@gmail.com>
2016-11-14 16:05:05 +0100

    years implement

--- years/years_module.py
+++ years/years_module.py
@@ -2,10 +2,16 @@ import datetime
 
 
 def years(age):
-    return
+    now = datetime.datetime.now()
+    return now.year - age + 100
 
 
 def main():
+    name = input("Pease enter your name: ")
+    age = int(input("Please enter your age: "))
+    howmanytimes = int(input("How many times you want to know this: "))
+    for i in range(howmanytimes):
+        print(name + ", you will be 100 years old in " + str(years(age)))
     return
 
 
